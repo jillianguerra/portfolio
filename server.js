ead900fe479de333c543061450fd66e113f82bf0
jillianguerra <guerrajilliant@gmail.com>
2023-09-28 16:40:13 -0400

    remove server.js

--- server.js
+++
@@ -1,7 +0,0 @@
-require('dotenv').config()
-const app = require('./app-server')
-const PORT = process.env.PORT || 3000
-
-app.listen(PORT, () => {
-    console.log(`Welcome to the danger zone ${PORT}`)
-})
