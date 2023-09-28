6629e38ff2c19af57880297648fa661d0f928194
jillianguerra <guerrajilliant@gmail.com>
2023-09-28 16:30:04 -0400

    adds github links to portfolio

--- app/components/PortfolioLinks/PortfolioLinks.jsx
+++ app/components/PortfolioLinks/PortfolioLinks.jsx
@@ -8,19 +8,23 @@ export default function PortfolioLinks() {
           <div>
             A cafe that lets users buy and rate Pokemon.
           </div>
+          <a className={styles.github} href="https://github.com/jillianguerra/mern-cafe">GitHub</a>
+          
         </div>
         <div className={styles.list}>
-          <a href="https://buckets-and-beyond.tech/">Buckets and Beyond</a>
+          <a  href="https://buckets-and-beyond.tech/">Buckets and Beyond</a>
           <div>
             A knock-off Home Depot website. Users can rate products, add products
             to wishlist, and buy products.
           </div>
+          <a className={styles.github} href="https://github.com/jillianguerra/home-depot">GitHub</a>
         </div>
         <div className={styles.list}>
             <a href="https://vocabulary-builder.jillianguerra.me">Vocabulary Builder</a>
           <div>
             A vocabulary building app that grabs words from a dictionary API, and, with a click of a button, puts those words into a list. Perfect for students.
           </div>
+          <a className={styles.github} href="https://github.com/jillianguerra/vocabulary-builder">GitHub</a>
         </div>
       </div>
     );
